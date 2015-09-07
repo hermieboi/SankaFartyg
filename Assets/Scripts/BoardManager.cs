b0a5bb47b4137578af8ec4e542787535c9dde1a1
hermieboi <herman_cool@hotmail.com>
2015-09-07 16:22:49 +0200

    Second Commit
    Re-made the cube script with OnMouseOver.
    Changed the position of the main camera
    Added new material to the cubes

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -35,7 +35,7 @@ public class BoardManager : MonoBehaviour
 			{
 				GameObject go = Instantiate (cubePrefab, new Vector3 (j,i,0f), Quaternion.identity) as GameObject;
 				go.GetComponent<MeshRenderer>().material.color = Color.blue;
-				go.name = ("Y: "+ i + ", " + "X: " + j).ToString();
+				go.name = ("X: "+ j + ", " + "Y: " + i).ToString();
 				go.transform.position = new Vector3(go.transform.position.x, go.transform.position.y, go.transform.position.z);
 				go.transform.SetParent(boardHolder1);
 				
@@ -46,16 +46,15 @@ public class BoardManager : MonoBehaviour
 	void EnemyBoard()
 	{
 		boardHolder2 = new GameObject ("EnemyBoard").transform;
-//		boardHolder2.transform.position = new Vector3 (11, 0, 0f)
 		for (int i = 0; i < heightY; i++) 
 		{
 			for (int j = 0; j < widthX ; j++)
 			{
-				GameObject go = Instantiate (cubePrefab, new Vector3 (j,i,0f), Quaternion.identity) as GameObject;
-				go.GetComponent<MeshRenderer>().material.color = Color.blue;
-				go.name = ("Y: "+ i + ", " + "X: " + j).ToString();
-				go.transform.position = new Vector3(go.transform.position.x + 11, go.transform.position.y, 0f);
-				go.transform.SetParent(boardHolder2);
+				GameObject go1 = Instantiate (cubePrefab, new Vector3 (j,i,0f), Quaternion.identity) as GameObject;
+				go1.GetComponent<MeshRenderer>().material.color = Color.blue;
+				go1.name = ("X: "+ j + ", " + "Y: " + i).ToString();
+				go1.transform.position = new Vector3(go1.transform.position.x + 12, go1.transform.position.y, 0f);
+				go1.transform.SetParent(boardHolder2);
 				
 			}
 		}
---
+++ Assets/Scripts/CubeManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeManager : MonoBehaviour 
+{
+
+	private Color baseColor;
+
+	// Use this for initialization
+	void Start () 
+	{
+		gameObject.GetComponent<MeshRenderer>().material.color = baseColor;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnMouseOver ()
+	{
+		gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+	}
+
+	void OnMouseExit ()
+	{
+		gameObject.GetComponent<MeshRenderer>().material.color = baseColor;
+	}
+}
