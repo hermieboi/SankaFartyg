fe07dd5b94b411d4de00dc00afbc614abd7a996b
hermieboi <herman_cool@hotmail.com>
2015-09-16 16:31:41 +0200

    Boat prefabs spawns when button is clicked
    trying to get the getComponent to work.
    debug.log stating how many ships left to spawn.

---
+++ Assets/Scripts/ChaseMouse.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaseMouse : MonoBehaviour 
+{
+	
+	private Vector3 mousePoz;
+	private float x_Cords;
+	private float y_Cords;
+	private SpawnManager _spawnManager;
+
+
+	
+	
+
+	void Awake()
+	{
+		_spawnManager = GetComponent<SpawnManager>();
+	}
+	// Use this for initialization
+	void Start () 
+	{
+
+		mousePoz = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		mousePoz = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 15);
+		mousePoz = Camera.main.ScreenToWorldPoint (mousePoz);
+		transform.position = mousePoz;
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Destroy(gameObject);
+			Debug.Log ("You just destroyed: " + gameObject.name + ".");
+			Debug.Log ("Placement canceled. You have 1 more " + gameObject.name + " to spawn.");
+
+		}
+		
+	}
+	
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class SpawnManager : MonoBehaviour 
 {
+	public GameObject SubmarinePrefab;
+	public GameObject DestroyerPrefab;
+	public GameObject CrusierPrefab;
+	public GameObject BattleshipPrefab;
+	public int Sub_total;
+	public int Destroyer_total;
+	public int Crusier_total;
+	public int Battleship_total;
 
 	// Use this for initialization
 	void Start () 
@@ -16,5 +24,37 @@ public class SpawnManager : MonoBehaviour
 	
 	}
 
+	public void SpawnSubmarine()
+	{
+		Sub_total = 4;
+		GameObject SubmarineClone = Instantiate (SubmarinePrefab, new Vector3 (0f,0f,0f), Quaternion.identity) as GameObject;
+		Sub_total --;
+		Debug.Log("You may spawn " + Sub_total + " more Submarines.");
+
+	}
+
+	public void SpawnDestroyer()
+	{
+		Destroyer_total = 3;
+		GameObject DestroyerClone = Instantiate (DestroyerPrefab, new Vector3 (0f,0f,0f), Quaternion.identity) as GameObject;
+		Destroyer_total --;
+		Debug.Log("You may spawn " + Destroyer_total + " more Destroyers.");
+	}
+
+	public void SpawnCrusier()
+	{
+		Crusier_total = 2;
+		GameObject CrusierClone = Instantiate (CrusierPrefab, new Vector3 (0f,0f,0f), Quaternion.identity) as GameObject;
+		Crusier_total --;
+		Debug.Log("You may spawn " + Crusier_total + " more Crusiers.");
+	}
+
+	public void SpawnBattleship()
+	{
+		Battleship_total = 1;
+		GameObject BattleshipClone = Instantiate (BattleshipPrefab, new Vector3 (0f,0f,0f), Quaternion.identity) as GameObject;
+		Battleship_total --;
+		Debug.Log("You may spawn " + Battleship_total + " more Battleships.");
+	}
 
 }
