0b7b4eea84a6d3d82b14bf685b8ad7e55414a563
hermieboi <herman_cool@hotmail.com>
2015-09-17 14:16:06 +0200

    Small commit before uninstall old unity version

--- Assets/Scripts/ChaseMouse.cs
+++ Assets/Scripts/ChaseMouse.cs
@@ -8,7 +8,7 @@ public class ChaseMouse : MonoBehaviour
 	private float x_Cords;
 	private float y_Cords;
 	private SpawnManager _spawnManager;
-
+	public bool Chasing = true;
 
 	
 	
@@ -27,18 +27,31 @@ public class ChaseMouse : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+
+		ChasingMouse();
+
+		
+	}
+
+	void OnMouseDown()
+	{
+		if (!Chasing)
+		{
+			gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, 15);
+		}
+	}
+
+	void ChasingMouse()
+	{
 		mousePoz = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 15);
 		mousePoz = Camera.main.ScreenToWorldPoint (mousePoz);
 		transform.position = mousePoz;
-
+		
 		if (Input.GetKeyDown(KeyCode.Escape))
 		{
 			Destroy(gameObject);
 			Debug.Log ("You just destroyed: " + gameObject.name + ".");
 			Debug.Log ("Placement canceled. You have 1 more " + gameObject.name + " to spawn.");
-
 		}
-		
 	}
-	
 }
